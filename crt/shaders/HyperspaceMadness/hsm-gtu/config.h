d6f338875069ad27799855604a808e0dd9379047
HyperspaceMadness <remimcgill@hotmail.com>
2020-04-26 17:12:19 -0400

    HSM Reflection V 2020-04-26

--- crt/shaders/HyperspaceMadness/hsm-gtu/config.h
+++ crt/shaders/HyperspaceMadness/hsm-gtu/config.h
@@ -1,3 +1,4 @@
-#pragma parameter compositeConnection "[GTU]  Composite Connection Enable" 0.0 0.0 1.0 1.0
+#pragma parameter gtu_on "[TV PROCESS GTU]  ON" 0.0 0.0 1.0 1.0
+#pragma parameter compositeConnection "[TV PROCESS GTU]  Composite Connection Enable" 0.0 0.0 1.0 1.0
 
 #define FIXNUM 6

c5207065414d8b8377626fa6cfe3de94e6f77527
HyperspaceMadness <remimcgill@hotmail.com>
2021-06-06 14:18:09 -0400

    Adjusted GTU params & Deconvergence On/Off

--- bezel/Mega_Bezel/shaders/HyperspaceMadness/hsm-gtu/config.h
+++ bezel/Mega_Bezel/shaders/HyperspaceMadness/hsm-gtu/config.h
@@ -1,4 +1,5 @@
-#pragma parameter GTU_TITLE		"[ --- TV PROCESS GTU --- ]:" 0 0 0.01 0.01
-#pragma parameter GTU_MODE      "          OFF | ON | Composite" 0 0 2 1
+#pragma parameter GTU_TITLE		            "[ --- TV PROCESS GTU --- ]:" 0 0 0.01 0.01
+#pragma parameter GTU_ON                  "          GTU ON" 0 0 1 1
+#pragma parameter compositeConnection "Composite Connection Enable" 0.0 0.0 1.0 1.0
 
 #define FIXNUM 6
